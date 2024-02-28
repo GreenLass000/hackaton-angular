b57ec495c878ad48ac554a4e45b8e289d5500c71
GreenLass <marcosnavarromartin@gmail.com>
2024-02-28 09:59:54 +0100

    rutas completas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,18 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AppComponent } from './app.component';
-
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 export const routes: Routes = [
     {
-        path:"login",
+        path: "login",
         component: LoginComponent
     },
     {
-        path:"home",
+        path: "home",
         component: AppComponent
+    }, {
+        path: "user-detail/:id",
+        component: UserDetailComponent
     }
 ];
